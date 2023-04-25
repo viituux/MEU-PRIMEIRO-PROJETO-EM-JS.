8b4fd2b5f9b6cd2c1badeb8dbe1171a835e702ca
JOAO VICTOR MARQUES DE LIMA DA LUZ <112225965+viituux@users.noreply.github.com>
2023-04-25 16:10:44 -0400

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,23 @@
+/*var currentNumber = document.getElementById(currentNumber)
+
+function encrement(){
+    currentNumber = currentNumber - 1; 
+}*/
+let count = 0;
+
+const CURRENT_NUMBER = document.getElementById('currentNumber');
+
+function increment() {
+	count++;
+	CURRENT_NUMBER.innerHTML = count;
+}
+
+function decrement() {
+	count--;
+	CURRENT_NUMBER.innerHTML = count;
+}
+
+function test() {
+	kdowkdpo;
+}
+
